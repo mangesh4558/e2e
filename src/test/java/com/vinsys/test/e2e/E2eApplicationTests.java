31338b4f690e1a5d9dbb9f723a03f4a5f963c139
root <root@localhost.localdomain>
2018-09-20 09:00:38 -0400

    intial commit

---
+++ src/test/java/com/vinsys/test/e2e/E2eApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vinsys.test.e2e;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class E2eApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
