9f3e96ee1df59af0744100371bf5080a8772fa96
root <root@localhost.localdomain>
2018-09-20 10:53:24 -0400

    change 6

--- src/main/java/com/vinsys/test/e2e/E2eApplication.java
+++ src/main/java/com/vinsys/test/e2e/E2eApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class E2eApplication {
 
 	public static void main(String[] args) { 
-		System.out.println("Mangesh Great ahhe");
+		System.out.println("Mangesh1 Great ahhe");
 		SpringApplication.run(E2eApplication.class, args);
 	}
 }
